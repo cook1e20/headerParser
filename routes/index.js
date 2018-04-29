9d41734a9f546c59a3fa77c23346d9e7bb43bb0a
Stephen Cooke <steve_cooke_20@hotmail.com>
2018-04-29 20:37:02 +0100

    initial beta

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ var getIP = require('ipware')().get_ip;
 router.get('/', function(req, res, next) {
  let response = {}
 
+let software = req.headers['user-agent'].match(/\(([^)]*)\)/)[1];
 
- response.ipaddress = getIP(req);
- response.language = #;
- response.software = #;
+ response.ipaddress = getIP(req).clientIp;
+ response.language = req.headers['accept-language'].split(',')[0];
+ response.software = software;
 
 
 
-
+console.log(software);
 
  res.json(response);
 
